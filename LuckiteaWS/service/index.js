39f3568cf28c04062bf63f96e714bd8dd81bd8f3
huny17 <honey17@byu.edu>
2024-12-03 14:35:24 -0700

    updated index in service

--- LuckiteaWS/service/index.js
+++ LuckiteaWS/service/index.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
 const DB = require('./database.js');
+const { peerProxy } = require('./peerProxy.js');
 
 const authCookieName = 'token';
 
@@ -110,3 +111,5 @@ function setAuthCookie(res, authToken) {
 const httpService = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+peerProxy(httpService);
